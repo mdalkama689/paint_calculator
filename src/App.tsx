5d34438e1a5842f0e0a0e249c5edb4969ba9bb20
mdalkama <mdalkama689@gmail.com>
2025-05-09 17:50:41 +0530

    add info icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import PaintCalculator from './components/PaintCalculator';
+import MobileShare from './components/share/MobileShare';
+import TabShare from './components/share/TabShare';
 
 function App() {
+  const [deviceType, setDeviceType] = useState<"big" | "small">("big");
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDeviceType(window.innerWidth < 1024 ? "small" : "big");
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
   return (
     <div className="min-h-screen bg-black text-gray-100">
+      {deviceType === "small" ? <MobileShare/> : <TabShare />}
       <div className="container mx-auto px-4 py-8 flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
